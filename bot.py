944a15081fd0472795d9c34a474df9a7492c435c
Kabs <lod.kabs@gmail.com>
2022-08-13 23:19:11 +0100

    Remove database usage, switching to separate file for now

--- bot.py
+++ bot.py
@@ -3,17 +3,18 @@ import os
 
 import collections
 import datetime
+from difflib import SequenceMatcher as SM
 from io import BytesIO
 import itertools
 from PIL import Image
-import psycopg2
 import requests
-import signal
 import textwrap
 
 import discord
 from dotenv import load_dotenv
 from discord.ext import tasks, commands
+
+from coffee_list import coffee_list
 
 print(str(datetime.datetime.now()) + '\n')
 
@@ -35,38 +36,7 @@ intents = discord.Intents.default()
 intents.members = True
 bot = commands.Bot(command_prefix='!', intents=intents)
 
-
-##### Database handling #####
-
-db = None
-cur = None
-
-try:
-    db = psycopg2.connect(
-            database= os.getenv('DB_NAME'),
-            user = os.getenv('DB_USERNAME'),
-            password = os.getenv('DB_PASSWORD'),
-            host = os.getenv('DB_HOST'),
-            port = os.getenv('DB_PORT')
-        )
-    cur = db.cursor()
-except Exception as e:
-    print(e)
-    print("\nCould not connect to database!\n")
-else:
-    print(f"Connected to {os.environ['DB_NAME']} database.\n")
-finally:
-    pass
-
-def keyboardInterruptHandler(signal, frame):
-    print(f"\nKeyboardInterrupt (ID: {signal}) has been caught.\n")
-    if db and cur:
-        cur.close()
-        db.close()
-        print(f"Disconnected from {os.environ['DB_NAME']} database.\n")
-    exit(0)
-
-signal.signal(signal.SIGINT, keyboardInterruptHandler)
+log_channel = bot.get_channel(channel_id['log'])
 
 
 ##### Functions #####
@@ -94,10 +64,11 @@ def drink_common_colour(url):
 
     res = requests.get(url)
     img = Image.open(BytesIO(res.content))
+    h = img.height
     img.convert('RGB')
 
     pixel_list = []
-    for x in itertools.product(range(19, 98), range(34, 61)):
+    for x in itertools.product(range(19, 98), range(h - 80, h - 54)):
         pixel_list.append(img.getpixel(x))
 
     colour_count = collections.Counter(pixel_list)
@@ -118,6 +89,16 @@ def seconds_until(hours, minutes):
     return (future_exec - now).total_seconds()
 
 
+def logging_in_channel(ctx, e, usage="Unexpected error"):
+    logging = f"Error detected at {str(datetime.datetime.now())}:"
+    logging += f"\n\tUser: {ctx.author.name}"
+    logging += f"\n\tChannel: {ctx.channel.name}"
+
+    logging += f"\n\n\tUsage: {usage}"
+    logging += f"\n\tReason: {e}"
+
+    return logging
+
 ##### Tasks #####
 
 @tasks.loop(hours=24)
@@ -135,27 +116,20 @@ async def my_job_forever(self):
 @bot.event
 async def on_ready():
     print(f'{bot.user.name} has connected to Discord!\n')
-    g = bot.guilds[0]
-    print(g.name)
-    async for m in g.fetch_members():
-        #print(f'{m} {m.id} {m.roles}')
-        print(f'{m} {m.id}')
-
-@bot.event
-async def on_member_join(member):
-    global db, cur
-
-    if db:
-        sql = f"""INSERT INTO discord_user (discord_user_id, discord_user_name)
-                  VALUES ('{member.id}', '{member}');"""
-        
-        cur.execute(sql)
-        db.commit()
+    # g = bot.guilds[0]
+    # print(g.name)
+    # async for m in g.fetch_members():
+    #     #print(f'{m} {m.id} {m.roles}')
+    #     print(f'{m} {m.id}')
 
 @bot.event
 async def on_command_error(ctx, error):
+    log_channel = bot.get_channel(channel_id['log'])
+
     if isinstance(error, commands.errors.CheckFailure):
         await ctx.send('You do not have the correct role for this command.')
+    else:
+        await log_channel.send(logging_in_channel(ctx, str(error)))
 
     print(error)
 
@@ -170,43 +144,61 @@ async def testing(ctx):
     await ctx.send(response)
 
 @bot.command(name='drink', help='Order a drink!')
-async def order_drink(ctx, drink_choice):
-    global db, cur
+async def order_drink(ctx, *args):
+    response = ""
+    em = None
 
-    sql = ''
-    try:
-        d_c = int(drink_choice)
-    except ValueError:
-        sql = f"""SELECT * FROM coffee WHERE
-                  LOWER(REPLACE(coffee_name, ' ', '')) LIKE
-                  LOWER('{drink_choice}%');"""
+    if args:
+        drink_choice = " ".join(args)
+        drink_rec = []
+
+        try:
+            d_c = int(drink_choice)
+        except ValueError:
+            for count, coffee in enumerate(coffee_list):
+                similarity_ratio = SM(isjunk=None, a=drink_choice, b=coffee["drink"]).ratio()
+                if similarity_ratio >= 0.5:
+                    drink_rec.append((count, coffee))
+        except Exception as e:
+            await log_channel.send(logging_in_channel(ctx, e, "Drink choice validation"))
+            response = "Umm, there is something wrong with the till, could you let a manager please?"
+        else:
+            if 0 < d_c <= len(coffee_list):
+                try:
+                    drink_rec.append((d_c - 1, coffee_list[d_c - 1]))
+                except IndexError:
+                    response = "Sorry, we don't have that many drinks!"
+                except Exception as e:
+                    await log_channel.send(logging_in_channel(ctx, e, "Coffee by number"))
+                    response = "Ah, there seems to be a problem with the coffee machine, could you get in touch with a manager please?"
+                finally:
+                    pass
+            else:
+                response = "That one is not on the menu!"
+        finally:
+            pass
+
+        if not response:
+            drink_len = len(drink_rec)
+            if drink_len == 1:
+                em = discord.Embed(
+                        title = textwrap.fill(drink_rec[0][1]["drink"], 17),
+                        description = textwrap.fill(drink_rec[0][1]["desc"], 23),
+                        color = drink_common_colour(drink_rec[0][1]["pic"])
+                        )
+                em.set_image(url = drink_rec[0][1]["pic"])
+                response = f"Here is your drink:"
+            elif drink_len > 1:
+                response = "My apologies, is the order one of the following?"
+                for d in drink_rec:
+                    response += f"\n\t{d[0] + 1}) {d[1]['drink']}"
+            else:
+                response = "That drink is unavailable"
+
     else:
-        sql = f"SELECT * FROM coffee WHERE coffee_id={d_c};"
-    finally:
-        pass
+        response = "Welcome! What drink would you like today?"
 
-    cur.execute(sql)
-    drink_rec = cur.fetchall()
-    drink_len = len(drink_rec)
-
-    e = None
-    if drink_len == 1:
-        e = discord.Embed(
-                title = drink_rec[0][1],
-                description = textwrap.fill(drink_rec[0][4], 23),
-                color = drink_common_colour(drink_rec[0][2])
-                )
-        e.set_image(url = drink_rec[0][2])
-        response = f"Here is your drink:"
-    elif drink_len > 1:
-        response = "My apologies, is the order one of the following?"
-        for d in drink_rec:
-            response += f"\n{d[0]} {d[1]}"
-        #await ctx.send(response)
-    else:
-        response = "That drink is unavailable"
-
-    await ctx.send(response, embed=e)
+    await ctx.send(response, embed=em)
 
 
 bot.run(TOKEN)
